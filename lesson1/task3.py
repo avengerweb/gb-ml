02c28f074936b2f5b05c5030b634ff3a4d5100a7
avengerweb <i@avenger-web.ru>
2019-02-17 23:04:57 +0300

    Lesson 1, task 3: Fix division by zero

--- lesson1/task3.py
+++ lesson1/task3.py
@@ -5,7 +5,12 @@ y1 = int(input("Enter the y coordinate of first point: "))
 x2 = int(input("Enter the x coordinate of second point: "))
 y2 = int(input("Enter the y coordinate of second point: "))
 
-k = (y1 - y2) / (x1 - x2)
+x_diff = x1 - x2
+k = 0
+
+if x_diff != 0:
+    k = (y1 - y2) / x_diff
+
 b = y2 - k * x2
 
 # I can add here several conditions for k and b and make pretty output of the equation, but i`m lazy (big flow-chart)
