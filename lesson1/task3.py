e9af7ee485eb358c2c8bc7696b618fbb42ef1f52
avengerweb <i@avenger-web.ru>
2019-02-17 17:17:26 +0300

    Lesson 1, task 3

---
+++ lesson1/task3.py
@@ -0,0 +1,12 @@
+# Diagram: https://drive.google.com/file/d/1kFg-w3n_3cYiaRjikXsFOlvMWkYwb5NO/view?usp=sharing
+x1 = int(input("Enter the x coordinate of first point: "))
+y1 = int(input("Enter the y coordinate of first point: "))
+
+x2 = int(input("Enter the x coordinate of second point: "))
+y2 = int(input("Enter the y coordinate of second point: "))
+
+k = (y1 - y2) / (x1 - x2)
+b = y2 - k * x2
+
+# I can add here several conditions for k and b and make pretty output of the equation, but i`m lazy (big flow-chart)
+print("y = %i * x + (%i)" % (k, b))
