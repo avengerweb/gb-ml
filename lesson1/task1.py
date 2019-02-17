bbab7474cd10ad465ea33e9be88c2b22c4652803
avengerweb <i@avenger-web.ru>
2019-02-17 14:19:34 +0300

    Lesson 1, task 1

---
+++ lesson1/task1.py
@@ -0,0 +1,12 @@
+# No validation required (good user)
+number = int(input("Input three-digit number: "))
+
+# We shouldn`t use arrays :(
+digit1 = number // 100
+digit2 = number // 10 % 10
+digit3 = number % 10
+
+dSum = digit1 + digit2 + digit3
+dProduct = digit1 * digit2 * digit3
+
+print("Sum of digit: %i\nProduct of digit: %i" % (dSum, dProduct))
