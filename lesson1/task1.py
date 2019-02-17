a0f2666a5f6e463d9bc871dcf101043c73f011c0
avengerweb <i@avenger-web.ru>
2019-02-17 17:25:28 +0300

    They like snake_case

--- lesson1/task1.py
+++ lesson1/task1.py
@@ -7,7 +7,7 @@ digit1 = number // 100
 digit2 = number // 10 % 10
 digit3 = number % 10
 
-dSum = digit1 + digit2 + digit3
-dProduct = digit1 * digit2 * digit3
+digits_sum = digit1 + digit2 + digit3
+digits_product = digit1 * digit2 * digit3
 
-print("Sum of digit: %i\nProduct of digit: %i" % (dSum, dProduct))
+print("Sum of digit: %i\nProduct of digit: %i" % (digits_sum, digits_product))
